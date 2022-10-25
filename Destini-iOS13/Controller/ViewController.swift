d487a9c70efbb6f38d696da024822ddd519bc0a4
Gusiken <yassii_g@hotmail.com>
2022-10-25 14:57:37 +0900

    要復讐

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+//    let story0 = "You see a fork in the road"
+//    let choice1 = "Take a left"
+//    let choice2 = "Take a right"
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+//        storyLabel.text = story0
+//        choice1Button.setTitle(choice1, for: .normal)
+//        choice2Button.setTitle(choice2, for: .normal)
+    
     }
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+//        if sender.currentTitle == storyBrain.getchoice1() {
+//            storyLabel.text = "You see a tiger."
+//        } else if sender.currentTitle == storyBrain.getchoice2() {
+//            storyLabel.text =  "You find a treasure chest."
+//        } else {
+//            print("error")
+//        }
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()        
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
 
 }
 
